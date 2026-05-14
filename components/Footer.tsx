3a8bee1a046c02a335c39a2769b25b9190a5e941
Ayslan Johnson <ayslanjohnson@gmail.com>
2026-05-14 18:07:47 -0300

    Aprimora o layout do Footer e Header: adiciona z-index e reorganiza a estrutura do Footer para melhor responsividade.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,17 +4,20 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-[#2d5a27] text-white w-full mt-auto ">
-      <div className="mx-auto px-4 py-2 md:py-3 flex justify-center items-center gap-5">
-        <span className="text-center text-xs leading-relaxed w-100">
-          <span className="font-bold">Patricia Valesca</span> - Elaboração e design
-        </span>
-        <span className="text-center text-xs leading-relaxed w-100">
-          <span className="font-bold">Ana Paula</span> - Desenvolvimento Web
-        </span>
-        <span className="text-center text-xs leading-relaxed shrink-0">
-          © {currentYear} - Todos os direitos reservados
-        </span>
+    <footer className="bg-[#2d5a27] text-white w-full mt-auto z-1000">
+      <div className="mx-auto px-4 py-2 md:py-3 flex flex-col md:flex-row text-center justify-center items-center gap-5">
+        <p>
+          <span className="text-center text-xs leading-relaxed w-100">
+            <span className="font-bold">Patricia Valesca</span> - Elaboração e design
+          </span>
+          <br className="block md:hidden"></br>
+          <span className="text-center md:mx-10 text-xs leading-relaxed w-100">
+            <span className="font-bold">Ana Paula</span> - Desenvolvimento Web
+          </span>
+          <span className="text-center hidden md:block text-xs leading-relaxed shrink-0">
+            © {currentYear} - Todos os direitos reservados
+          </span>
+        </p>
       </div>
     </footer>
   );
